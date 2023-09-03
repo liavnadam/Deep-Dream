18fb180a87d5d15eba769cd4ab32be50479d9910
liavnadam <liav0508@cs.colman.ac.il>
2023-09-04 02:37:55 +0300

    cart and catalog

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-
+import {PRODUCTS} from "../../products";
+import {Products} from "./product";
+import "./shop.css";
 export const Shop = () => {
     return(
         <div className="shop">
         <div className="shopTitle">
             <h1>Deep Dream Shop</h1>
         </div>
-        <div className="products"></div>
+        <div className="products">
+            {" "}
+            {PRODUCTS.map((product) => (
+                <Products data={product}/>
+                
+            ))}
+            </div>
+
         </div>
     );
 };
---
+++ src/products.js
@@ -0,0 +1,27 @@
+import product1 from '../src/assets/16_wood_samples.jpg';
+import product2 from '../src/assets/canvas.jpeg';
+import product3 from '../src/assets/shirt.jpeg';
+
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Wood",
+        price: 100,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Canvas",
+        price: 200,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Shirt",
+        price: 50,
+        productImage: product3,
+    }
+    
+]
